cdc99907b8ba229dce21f17c804c666487462388
Ioan Avramov <avramovioan@gmail.com>
2023-02-15 15:05:56 +0200

    fixed typo

--- SlotMachine/Program.cs
+++ SlotMachine/Program.cs
@@ -6,11 +6,11 @@ namespace SlotMachine
     {
         //Can add more symbols. Just follow 2 conditions:
         //1. must be ordered by probability in DESC order
-        //1. the sum of all probabilities must equal 100(%)
+        //2. the sum of all probabilities must equal 100(%)
         public static readonly List<Symbol> Symbols = new List<Symbol>
         {
             new Symbol('A', 0.4, 45),
-            new Symbol('B', 0.8, 25),
+            new Symbol('B', 0.8, 35),
             new Symbol('C', 1.0, 15),
             new Symbol('*', 0.0, 5, true),
         };
